4af29e6358463c01530d796d4104253fcf7efd35
ares2355 <ares2355@mail.ru>
2023-02-17 23:53:02 +0300

    v2.0

--- binance.py
+++ binance.py
@@ -1,55 +1,47 @@
-import requests
-from bs4 import BeautifulSoup as bs
-import schedule
 from datetime import datetime
 
+import requests
+import schedule
+from bs4 import BeautifulSoup as bs
 
-def get_current_price():
+
+def update_current_price():
+    # Находим: максимальная цена за последний час
+    max_price = None
+
+    last_hour_prices = []
+    for dt, price in prices_history.items():
+        hours_ago = datetime.now() - dt
+        if hours_ago.seconds / 3600 <= 1:
+            last_hour_prices.append(price)
+    if len(last_hour_prices) > 0:
+        max_price = max(last_hour_prices)
+
+    # Парсим текущую цену
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
     response = requests.get('https://www.binance.com/ru/markets/futures', headers=headers)
-    soup = bs(response.text, 'lxml')
-    xrp_usdt = soup.find_all('div', id='market_trade_list_item')
-    for i in xrp_usdt:
-        if 'XRPUSDT' in i.text:
-            now_price = float(i.find('div', class_='css-18cwbk1').text.replace(',', '.'))
-            # history_price[now_time.strftime("%d-%m-%Y %H:%M")] = now_price
-            history_price.append({now_time.strftime("%d-%m-%Y %H:%M"): now_price})
-            count =+ 1
-        return now_price
+    soup = bs(response.text, 'html.parser')
+    market_trade_list = soup.find_all('div', id='market_trade_list_item')
+    now_price = None
+    for elm in market_trade_list:
+        if 'XRPUSDT' in elm.text:
+            now_price = float(elm.find('div', class_='css-18cwbk1').text.replace(',', '.'))
+            prices_history[datetime.now()] = now_price
+            break
 
-
-def get_last_price():
-    if count == 59:
-        get_current_price()
-        last_price = history_price[-1]
-        now_price = history_price[0]
-        for i in last_price:
-            last_price = last_price[i]
-            for v in now_price:
-                now_price = now_price[v]
-        if 100 - (last_price * 100 / now_price) == 1:
-            history_price.clear()
-        else:
-            return 0
+    # Сравниваем макс цену и текущую цену
+    # if 100 - (now_price * 100 / max_price) >= 1:
+    if max_price is not None and (max_price - now_price) >= max_price * 0.01:
+        print('Изменилась цена на 1% или более!')
+    print(f'{datetime.now().strftime("%H:%M:%S")} Макс цена за прошлый период: {max_price} Текущая цена: {now_price}')
 
 
 if __name__ == '__main__':
-    count = 0
-    now_time = datetime.now()
-    history_price = []
-    schedule.every(1).minutes.do(get_current_price)
+    prices_history = {}
+    schedule.every(1).seconds.do(update_current_price)
 
     while True:
-        if get_last_price() != 0:
-            print('Current change on 1%')
         schedule.run_pending()
-# li = []
-# for i in map(lambda x: x, xrp_usdt):
-#     if 'XRPUSDT' in i.text:
-#         li.append(i.text.strip('XRPUSDTбессрочный M–ИсторияТорговать$').split('/'))
-# current_price = li[0][0]
-#
-# print(current_price)
